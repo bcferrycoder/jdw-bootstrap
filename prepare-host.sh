40e8e7043f170ee0c3040e696298e67f20fb632a
jdw@bcferrycoder.com <jdw@bcferrycoder.com>
2014-02-14 10:19:15 -0800

    update for latest docker, add docker group

--- prepare-host.sh
+++ prepare-host.sh
@@ -4,36 +4,44 @@ USER=jdw
 
 # run this on ubuntu 13.04 host to install docker
 
+apt-get install -q -y git-core
+
+apt-get update
+apt-get install linux-image-extra-`uname -r`
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+sh -c "echo deb http://get.docker.io/ubuntu docker main\
+> /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
-sudo apt-get -y install linux-image-extra-`uname -r`
+sudo apt-get install lxc-docker
 
-# Add the Docker repository key to your local keychain
-# using apt-key finger you can check the fingerprint matches 36A1 D786 9245 C895 0F96 6E92 D857 6A8B A88D 21E9
-sudo sh -c "curl https://get.docker.io/gpg | apt-key add -"
-
-# Add the Docker repository to your apt sources list.
-sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-
-# update
-sudo apt-get update
-
-# install
-sudo apt-get -y install lxc-docker
 
 # verify
-sudo docker version
-sudo docker run -i -t ubuntu /bin/bash -c echo docker installed
+docker version
+docker run -i -t ubuntu /bin/bash -c echo docker installed
 
-sudo apt-get install -y git-core
-
-sudo useradd -d /home/${USER} -c "${USER}" -s /bin/bash ${USER}
-sudo git clone https://github.com/bcferrycoder/jdw-bootstrap.git /home/${USER}
-sudo mkdir -p /home/${USER}/.git /home/${USER}/.ssh
-sudo cp ~ubuntu/.ssh/authorized_keys /home/${USER}/.ssh
-sudo chmod -R 700 /home/${USER}/.ssh
-sudo chown -R ${USER}.${USER} /home/${USER}
+useradd -d /home/${USER} -c "${USER}" -s /bin/bash ${USER}
+git clone https://github.com/bcferrycoder/jdw-bootstrap.git /home/${USER}
+mkdir -p /home/${USER}/.git /home/${USER}/.ssh
+cp ~ubuntu/.ssh/authorized_keys /home/${USER}/.ssh
+chmod -R 700 /home/${USER}/.ssh
+chown -R ${USER}.${USER} /home/${USER}
 echo "${USER} ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers > /dev/null
-sudo cp config/john-gitconfig /home/${USER}/.git/config
+cp config/john-gitconfig /home/${USER}/.git/config
+
+# Add the docker group if it doesn't already exist.
+sudo groupadd docker
+
+# Add the connected user "${USER}" to the docker group.
+# Change the user name to match your preferred user.
+# You may have to logout and log back in again for
+# this to take effect.
+sudo gpasswd -a ${USER} docker
+
+# Restart the Docker daemon.
+sudo service docker restart
+
 
 echo 'this host is now docker-ready'
 echo "type \"sudo su - ${USER} to proceed\""
